1599cb3e8acf041eeb2cf31aa05881c96dae7fce
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-08-18 12:02:05 +0530

    checking

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const device = require("device");
 
 const app = express();
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 // Define a route for generating and redirecting dynamic invite links
 
-app.get("/invite", (req, res) => {
+app.get("/", (req, res) => {
   try {
     const userAgent = req.headers["user-agent"];
 
@@ -17,21 +18,21 @@ app.get("/invite", (req, res) => {
     let destinationURL = "https://platinx.exchange/exc/signup";
 
     // Update destination URL based on detected device
-    if (userDevice.includes("bot")) {
+    if (userDevice.is("bot")) {
       // Handling bots if needed
       res.send("Bot detected.");
       return;
-    } else if (userDevice.includes("Android")) {
+    } else if (userDevice.is("Android")) {
       destinationURL =
         "https://play.google.com/store/apps/details?id=com.platinx.exchange.flutter_platinx_exchange";
-    } else if (userDevice.includes("iPhone") || userDevice.includes("iPad")) {
+    } else if (userDevice.is("iPhone") || userDevice.is("iPad")) {
       destinationURL = "https://apps.apple.com/in/app/platinx/id1637866256";
-    } else if (userDevice.includes("Windows")) {
+    } else if (userDevice.is("Windows")) {
       // Handle desktop redirection
       destinationURL = "https://platinx.exchange/exc/signup";
     } else {
       // Redirect to the appropriate destination URL
-      res.redirect("https://platinx.exchange/exc/signup");
+      res.redirect(destinationURL);
     }
   } catch (error) {
     console.log("error", error);
@@ -42,3 +43,6 @@ app.get("/invite", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
