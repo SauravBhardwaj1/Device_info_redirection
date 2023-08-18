c209ddb9f5aaa5c6ea90e689c07f00b3b77aa643
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-08-18 11:59:10 +0530

    checking

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const express = require("express");
+const device = require("device");
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Define a route for generating and redirecting dynamic invite links
 
-app.get("/", (req, res) => {
+app.get("/invite", (req, res) => {
   try {
     const userAgent = req.headers["user-agent"];
+
+    const userDevice = device(userAgent);
 
     // Default destination URL
     let destinationURL = "https://platinx.exchange/exc/signup";
 
     // Update destination URL based on detected device
-    if (userAgent.includes("bot")) {
+    if (userDevice.includes("bot")) {
       // Handling bots if needed
       res.send("Bot detected.");
       return;
-    } else if (userAgent.includes("Android")) {
+    } else if (userDevice.includes("Android")) {
       destinationURL =
         "https://play.google.com/store/apps/details?id=com.platinx.exchange.flutter_platinx_exchange";
-    } else if (userAgent.includes("iPhone") || userAgent.includes("iPad")) {
+    } else if (userDevice.includes("iPhone") || userDevice.includes("iPad")) {
       destinationURL = "https://apps.apple.com/in/app/platinx/id1637866256";
-    } else if (userAgent.includes("Windows")) {
+    } else if (userDevice.includes("Windows")) {
       // Handle desktop redirection
       destinationURL = "https://platinx.exchange/exc/signup";
     } else {
