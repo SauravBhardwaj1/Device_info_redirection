3325d17a75268e1d1902a5b1ba4d6e58e8500e01
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-08-18 11:26:44 +0530

    adding backend

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const device = require('device');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,33 +7,34 @@ const PORT = process.env.PORT || 3000;
 // Define a route for generating and redirecting dynamic invite links
 
 app.get('/', (req, res) => {
-    const userAgent = req.headers['user-agent'];
-    const userDevice = device(userAgent);
+    const userAgent = req.headers['user-agent'];    
 
     // Default destination URL
-    let destinationURL = 'https://platinx.exchange/exc/signup';
+    let destinationURL = '';
 
     // Update destination URL based on detected device
-    if (userDevice.is('bot')) {
+    if (userAgent.includes('bot')) {
         // Handling bots if needed
         res.send('Bot detected.');
         return;
-    } else if (userDevice.is('Android')) {
+    } else if (userAgent.includes('Android')) {
         destinationURL = 'https://play.google.com/store/apps/details?id=com.platinx.exchange.flutter_platinx_exchange'; 
     }
-    else if (userDevice.is('iPhone') && userDevice.is('iPad')) {
+    else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
         destinationURL = 'https://apps.apple.com/in/app/platinx/id1637866256'; 
-    } else if (userDevice.is('desktop')) {
+    } else if (userAgent.includes('desktop')) {
         // Handle desktop redirection
         destinationURL = 'https://platinx.exchange/exc/signup'; 
     }
-    else if (userDevice.is('windows')) {
+    else if (userAgent.includes('Windows')) {
         // Handle desktop redirection
         destinationURL = 'https://platinx.exchange/exc/signup'; 
+    }else{
+        // Redirect to the appropriate destination URL
+        res.redirect("https://platinx.exchange/exc/signup");
     }
 
-    // Redirect to the appropriate destination URL
-    res.redirect(destinationURL);
+   
 });
 
 // Start the server
